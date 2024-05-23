d004ce8abdd2b03202d1961c3c1763e5d52af713
Reydel Ocon <reydel321@gmail.com>
2024-05-23 23:32:25 +0800

    major update

---
+++ monthly-orders-driving.php
@@ -0,0 +1,37 @@
+<?php
+
+
+include 'DBconnect.php';
+$objDB = new DbConnect();
+$conn = $objDB->connect();
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+switch ($method) {
+    case "GET":
+
+
+        $sql = "SELECT 
+        MONTHNAME(created_at) AS name, 
+        COUNT(*) AS total
+    FROM 
+        order_range
+    GROUP BY 
+        MONTH(created_at), 
+        MONTHNAME(created_at)
+    ORDER BY 
+        MONTH(created_at)";
+
+
+        if (isset($sql)) {
+            $stmt = $conn->prepare($sql);
+
+            $stmt->execute();
+            $product = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            echo json_encode($product);
+        }
+
+
+        break;
+}
